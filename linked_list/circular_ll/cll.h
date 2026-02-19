c4846e82a549e74fdc2a6fcee60e4baffbf7a731
seivarya <seivarya.he@gmail.com>
2026-02-19 20:27:02 +0530

    feat: implemented doubly linked list and circular linked list with similar structure of singly linked list with basic test

---
+++ linked_list/circular_ll/cll.h
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node_t;
+
+struct node {
+	int data;
+	node_t *next;
+	node_t *prev;
+};
+
+typedef struct circular_ll {
+	node_t *head;
+	node_t *tail; // the list keeps track of head as well as the tail pointer
+	size_t length;
+} circular_ll;
+
+
+circular_ll* circular_init(void);
+node_t* circular_iter(circular_ll *list, size_t index);
+
+int circular_insert(circular_ll *list, size_t index, int data);
+void circular_remove(circular_ll *list, size_t index);
+void circular_print(circular_ll *list);
+
+
---
+++ linked_list/doubly_ll/dll.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node_t;
+
+struct node {
+	int data;
+	node_t *next;
+	node_t *prev;
+};
+
+typedef struct doubly_ll {
+	node_t *head;
+	node_t *tail; // the list keeps track of head as well as the tail pointer
+	size_t length;
+} doubly_ll;
+
+
+doubly_ll* doubly_init(void);
+node_t* doubly_iter(doubly_ll *list, size_t index);
+
+int doubly_insert(doubly_ll *list, size_t index, int data);
+void doubly_remove(doubly_ll *list, size_t index);
+void doubly_print(doubly_ll *list);
+
+
+
