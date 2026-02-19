0d43ca7d150ea077e7c771283f4a7839c5e46ac0
seivarya <seivarya.he@gmail.com>
2026-02-19 11:49:52 +0530

    feat: implemented singly linked list and restrucrued files in linked_list directory

---
+++ linked_list/singly_ll/sll.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node_t;
+
+struct node {
+	int data;
+	node_t *next;
+};
+
+typedef struct singly_ll {
+	node_t *head;
+	size_t length;
+} singly_ll;
+
+
+singly_ll* singly_init(void);
+node_t* singly_iter(singly_ll *list, size_t index);
+
+int singly_insert(singly_ll *list, size_t index, int data);
+void singly_remove(singly_ll *list, size_t index);
+void singly_print(singly_ll *list);
+
+
+
