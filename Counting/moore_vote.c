bbb0bce4c36097e04e2c4bc675cff9381498b8c7
dubistweltmeister05 <kshitijvaze@gmail.com>
2026-03-05 12:40:26 +0530

    Cleaned up the code on the moore_validation code

--- Counting/moore_vote.c
+++ Counting/moore_vote.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-
     int test[] = {1, 1, 1, 1, 2, 3, 5};
+    int n = sizeof(test) / sizeof(test[0]);
 
     int candidate = -1;
     int vote = 0;
 
-    for (int i = 0; i < sizeof(test) / sizeof(test[0]); i++)
+    for (int i = 0; i < n; i++)
     {
         if (vote == 0)
-        {
             candidate = test[i];
-            vote = 1;
-        }
 
         if (test[i] == candidate)
             vote++;
         else
             vote--;
     }
+
     int count = 0;
-    for (int i = 0; i < sizeof(test) / sizeof(test[0]); i++)
-    {
+    for (int i = 0; i < n; i++)
         if (test[i] == candidate)
             count++;
-    }
-    printf( "%d", (count>((sizeof(test) / sizeof(test[0])/2)))?count:-1);
 
-    // return 0;
+    printf("%d\n", (count > n/2) ? candidate : -1);
+
+    return 0;
 }
