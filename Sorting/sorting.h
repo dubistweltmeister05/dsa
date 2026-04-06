2f2f7e1a704ea58121b070886f6bf6ad4ac2b657
Kshitij <kshitij.vaze@tor.ai>
2026-04-06 17:28:06 +0530

    Added merge sort code

--- Sorting/sorting.h
+++ Sorting/sorting.h
@@ -12,6 +12,8 @@ enum sorting {
 int bubble_sort(int *arr, int size);
 int selection_sort(int *arr, int size);
 int insertion_sort(int *arr, int size);
+void mergeSort(int arr[], int left, int right);
+void merge(int arr[], int left, int mid, int right);
 void print_ele(int *arr, int sie);
 
 #endif
